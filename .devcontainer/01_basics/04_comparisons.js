7b12d194a35426631c904a7e97101d4d8eb22fa3
Susmita-Khan <162450279+Susmita-Khan@users.noreply.github.com>
2024-03-07 15:07:54 +0000

    comparisons operator

---
+++ .devcontainer/01_basics/04_comparisons.js
@@ -0,0 +1,6 @@
+// console.log("2" > 1);
+console.log(null > 0); //
+console.log(null == 0); //
+console.log(null >= 0); // equality check; Avoid these type of checks redundancy in it.
+
+console.log("2" === 2); // Strict check.
