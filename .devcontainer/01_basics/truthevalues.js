acb719d0fea38c2168670ae76f6b1c1bd4a3a2c9
Susmita-Khan <162450279+Susmita-Khan@users.noreply.github.com>
2024-03-17 06:13:45 +0000

    truthyand falsy values

---
+++ .devcontainer/01_basics/truthevalues.js
@@ -0,0 +1,39 @@
+/*const userEmail = []
+if (userEmail) {
+    console.log("Got user Email");
+} else {
+    console.log("Don't have user Email");
+} */
+/* const userEmail = ""
+if (userEmail) {
+    console.log("Got user Email");
+} else {
+    console.log("Don't have user Email");
+} */
+// falsy values 
+// false, 0, -0,BigInt On,"",null,undefined,Nan
+
+//Truthy values
+//"0",'false', " ",[],{},function(){}
+/* const userEmail = []
+if (userEmail.length===0) {
+    console.log("Array is Empty");
+} */
+
+/* const emptyobj = {}
+if (Object.keys(emptyobj).length === 0) {
+    console.log("object is empty");
+} */
+// Nullish coalescing Operator (??)
+
+/* let val1;
+//val1 = undefined ?? 15
+val1 = 5 ?? 15
+console.log(val1); */
+
+// Ternary Operator
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
